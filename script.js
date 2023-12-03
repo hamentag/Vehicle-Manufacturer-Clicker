46a2f0e7db70308faff9519ae94b072ec63180cd
Hamza Amentag <hamentag1@gmail.com>
2023-12-03 00:35:24 -0600

    Updated Product and Producer classes and added the render function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const data = window.data;
 
 // References
-const carProdContainer = document.getElementById('car-producers');
+const producersContainer = document.getElementById('car-producers');
 
 
 const carIcon = document.getElementById('car-icon');
@@ -16,18 +16,35 @@ class Product {
     this.totalUPPUT = _product.totalUPPUT;  // total units produced per unit time
   }
   getCount() { return this.count; }
-  getTotalUPPUT() { return this.totalUPPUT; }   
+  getTotalUPPUT() { return this.totalUPPUT; }
 
-//   updateDisplayedCount(){
-    
-//   }
-
-  clickCar(){
-    this.count += 1;
+  updateDisplayedCount(){
     const productCounter = document.getElementById('counter');
     productCounter.innerText = this.count;
   }
+  
+  updateDisplayedTotalUPPUT(){
+    const cpsDiv = document.getElementById(`${this.name}-UPPUT`);
+    cpsDiv.innerText = this.totalUPPUT;
+  }
 
+  clickCar(){
+    this.count += 1;
+    this.updateDisplayedCount();
+  }
+  deductCost(cost){
+    this.count -= cost; 
+  }
+
+  updateDisplayedTotalUPPUT(){
+    const upputDiv = document.getElementById('UPPUT');
+    upputDiv.innerText = this.totalUPPUT;
+  }
+
+  increaseThroughput(increase){
+    this.totalUPPUT += increase;
+    this.updateDisplayedTotalUPPUT();
+  }
 }
 
 class Producer { 
@@ -39,16 +56,90 @@ class Producer {
     this.qty = _producer.qty;
   }
 
+  setUnlocked(count){
+    if(count >= this.price / 2){
+      this.unlocked = true;
+    }
+  }
+
+  makeDisplayNameFromId() {
+    return this.id.split("_")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+  }
+
+  makeProducerDiv() {
+    if(this.unlocked){
+      const containerDiv = document.createElement("div");
+      containerDiv.className = "producer";
+      const html = `
+        <div class="producer-column">
+          <div class="producer-title">${this.makeDisplayNameFromId()}</div>
+        </div>
+        <div class="producer-column">
+          <div>Quantity: ${this.qty}</div>
+          <div>Unit/second: ${this.cps}</div>
+          <div>Cost: ${this.price} Unit</div>
+        </div>
+      `;
+      containerDiv.innerHTML = html;
+
+      const btn = document.createElement('button');
+      btn.type = "button";
+      btn.innerHTML = "Buy";
+      btn.id = `buy_${this.id}`;
+      btn.addEventListener('click', (event) => {buyButton_Click(event);});
+
+      containerDiv.append(btn);
+      return containerDiv;
+    }
+    return '';    
+  }
+  attemptToBuyProducer(count){
+    if(this.price <= count){
+      car.deductCost(this.price);
+      this.qty += 1;
+      this.price = Math.floor(this.price * 1.25);
+      car.increaseThroughput(this.cps);
+    }
+    else{
+      window.alert("Not enough produced units!");      
+    } 
+
+}
 }
 
 ///////
 const car = new Product(data);
 
-console.log("Create instances...")
 const carProducers = data.producers.map(producer => {
   return new Producer(producer);
 });
 
-console.log(carProducers);
+//////////
 
-//////////////////
+function render(){  
+    const producers = carProducers.map(producer => {
+      producer.setUnlocked(car.getCount());
+      return producer.makeProducerDiv();
+    }); 
+    producersContainer.replaceChildren(...producers);
+  }
+
+  ///  
+  function buyButton_Click(event) {
+    const producerId = event.target.id.slice(4);
+    const selectedProducer = carProducers.find(producer => producer.id === producerId);
+    selectedProducer.attemptToBuyProducer(car.getCount());
+    render();
+  }
+  
+  ///
+  function tick() {
+    car.count += car.totalUPPUT;
+    car.updateDisplayedCount();
+    render();
+  }
+  
+  // Repeat the tick function every 1000ms, or 1s
+  setInterval(() => tick(), 1000);
